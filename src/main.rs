8b430bee6c90be49ef95c764ee4a197c79338da0
GGORG <GGORG0@protonmail.com>
2024-08-15 11:55:18 +0200

    added logging via `log` and `wasm-logger`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use log::{error, info};
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
+    info!("Some info");
+    error!("Error message");
+
     html! {
       <h1>
         {"Hello world!"}
@@ -10,5 +14,6 @@ fn App() -> Html {
 }
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
 }
